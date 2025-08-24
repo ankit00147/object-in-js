c5836b1a6aa306adaa70a964100cd2e917cbf0e3
ankit chandel <chandelankit1313@gmail.com>
2025-08-24 23:24:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let student ={
+    name: "ankit chandel",
+    course: "B-tech",
+    branch: "cyber security",
+    enrollment_no: "0187CY241006",
+    college: "SISTec",
+    year: "second year",
+}
+console.log(student);
